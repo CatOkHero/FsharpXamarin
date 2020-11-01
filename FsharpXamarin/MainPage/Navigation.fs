45ea9ce871f81a7a8da1cac1c2510695711f127d
CatOkHero <roman.hapatyn@gmail.com>
2020-11-01 11:37:26 +0200

    Added basic navigation (wrong navigation))

---
+++ FsharpXamarin/MainPage/Navigation.fs
@@ -0,0 +1,57 @@
+﻿namespace FsharpXamarin.MainPage
+
+open Fabulous
+open Fabulous.XamarinForms
+open FsharpXamarin.Pages
+open Xamarin.Forms
+
+module Navigation = 
+    type Msg =
+        | GoToLoginPage
+        | GoHomePage
+        | PopPage 
+        | PagePopped 
+        | ReplacePage of string
+        | PushPage of string
+
+    type CmdMsg = Nothing
+
+    type Model =
+        { LoggedIn: bool
+          PageStack: string option list }
+
+    let mapToCmd _ = Cmd.none
+
+    let init () =
+        { LoggedIn = false; PageStack = [Some HomePage.Tag] }
+
+    let update msg model =
+        match msg with        
+        | GoToLoginPage ->
+            { model with PageStack = [ Some LoginPage.Tag ] }, []
+        | GoHomePage -> 
+            { model with PageStack = [ Some HomePage.Tag ] }, []
+        | PagePopped -> 
+            if model.PageStack |> List.exists Option.isNone then 
+                { model with PageStack = model.PageStack |> List.filter Option.isSome }, []
+            else
+                { model with PageStack = (match model.PageStack with [] -> model.PageStack | _ :: t -> t) }, []
+        | PopPage -> 
+            { model with PageStack = (match model.PageStack with [] -> model.PageStack | _ :: t -> None :: t) }, []
+        | PushPage page -> 
+            { model with PageStack = Some page :: model.PageStack}, []
+        | ReplacePage page -> 
+            { model with PageStack = (match model.PageStack with [] -> Some page :: model.PageStack | _ :: t -> Some page :: t) }, []
+
+    let view msg dispatch =
+
+       View.NavigationPage(
+            popped = (fun _ -> dispatch PagePopped), 
+            poppedToRoot = (fun _ -> dispatch GoHomePage),
+            pages = [
+                if not(msg.LoggedIn) then
+                    yield (LoginPage.view dispatch)
+                else 
+                    yield (HomePage.view dispatch)
+            ]
+        )
---
+++ FsharpXamarin/Pages/LoginPage.fs
@@ -0,0 +1,85 @@
+﻿namespace FsharpXamarin.Pages
+
+open System
+open Fabulous
+open Fabulous.XamarinForms
+open Xamarin.Forms
+
+module LoginPage =
+    let [<Literal>] Tag = "Login"
+
+    type Msg =
+        | LoginFailed
+        | LoginSuccessFull
+        | LoginValidating
+        | ShowErrorMessage of string
+        | DoNothing
+
+    type Model =
+        { Username: string
+          Password: string
+          IsLoading: bool
+          LoginFailed: bool
+          LoginFailedMessage: string }
+
+    let init =
+        { Username = String.Empty; 
+          Password = String.Empty; 
+          IsLoading = false; 
+          LoginFailed = false; 
+          LoginFailedMessage = String.Empty }
+
+    let update msg (model : Model) =
+        match msg with
+        | LoginFailed ->
+            { model with LoginFailed = true }, Cmd.none
+        | LoginSuccessFull ->
+            
+            model, Cmd.none
+        | LoginValidating ->
+            match model.Username, model.Password with
+            | "", "" ->
+                model, Cmd.none
+            | _, _ ->
+                model, Cmd.none
+        | ShowErrorMessage e ->
+            model, Cmd.none
+        | DoNothing -> 
+            model, Cmd.none
+
+    let view dispatch =
+        let userName =
+            View.Entry(
+                keyboard = Keyboard.Plain,
+                placeholder = "Username or email")
+
+        let usernameErrors = 
+            View.Label()
+
+        let password =
+            View.Entry(
+                isPassword = true,
+                placeholder = "Password")
+
+        let passwordErrors =
+            View.Label()
+
+        let loginButton =
+            View.Button(
+                text = "Login",
+                command = (fun () -> dispatch LoginSuccessFull))
+
+        View.ContentPage(
+            content = 
+                View.StackLayout(
+                    verticalOptions = LayoutOptions.CenterAndExpand,
+                    children = [
+                        //lottie
+                        userName
+                        usernameErrors
+                        password
+                        passwordErrors
+                        loginButton
+                    ])
+        )
+
